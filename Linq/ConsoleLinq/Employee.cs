dc579726e054b55a0337475276d888a41a061ab0
levj1 <levj1@yahoo.com>
2016-12-15 15:08:31 -0500

    linq practice hello  world

---
+++ Linq/ConsoleLinq/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLinq
+{
+    public class Employee
+    {
+        public int id;
+        public string firstname;
+        public string lastname;
+
+        public static ArrayList GetEmployees()
+        {
+            ArrayList al = new ArrayList();
+            al.Add(new Employee { id = 1, firstname = "Joe", lastname = "Rattz" });
+            al.Add(new Employee { id = 2, firstname = "William", lastname = "Gates" });
+            al.Add(new Employee { id = 1, firstname = "Anders", lastname = "Hejlsberg" });
+
+            return(al);
+        }
+    }
+
+    public class Contact
+    {
+        public int id;
+        public string Name;
+
+        public static void PublishContacts(Contact[] contacts)
+        {
+            foreach (Contact c in contacts)
+            {
+                Console.WriteLine("Contact Id: {0} Contact: {1}", c.id, c.Name);
+            }
+        }
+    }
+}
---
+++ Linq/ConsoleLinq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] greeting = { "hello world", "linq", "hello linq", "hello apress" };
+            var items =
+                from s in greeting
+                where s.StartsWith("linq")
+                select s;
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
